0ba57969f435bb7392c451d13cbdb6acc137fad4
anupam1262000 <anupam.1262000@gmail.com>
2018-12-09 05:00:19 +0530

    modified second activity

--- app/src/main/java/com/mapuna/com/succotash/MainActivity.java
+++ app/src/main/java/com/mapuna/com/succotash/MainActivity.java
@@ -1,14 +1,17 @@
 package com.mapuna.com.succotash;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.view.Window;
 import android.view.animation.BounceInterpolator;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.sdsmdg.harjot.rotatingtext.RotatingTextWrapper;
 import com.sdsmdg.harjot.rotatingtext.models.Rotatable;
@@ -17,18 +20,29 @@ import com.sdsmdg.tastytoast.TastyToast;
 public class MainActivity extends AppCompatActivity {
 
     Button b;
+    ImageView i;
+    com.sdsmdg.harjot.rotatingtext.RotatingTextWrapper rt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rt=(com.sdsmdg.harjot.rotatingtext.RotatingTextWrapper)findViewById(R.id.custom_switcher);
         b=(Button)findViewById(R.id.getst);
+        i=(ImageView)findViewById(R.id.logo);
+
+        final Pair p[]=new Pair[2];
+                p[0]=new Pair<View, String>(rt,"bth");
+                p[1]=new Pair<View, String>(i,"log");
 
         b.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,secondactivity.class));
+
+                ActivityOptions options= ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,p);
+                startActivity(new Intent(MainActivity.this,secondactivity.class),options.toBundle());
                 //TastyToast.makeText(getApplicationContext(), "UNDER BUILD!", TastyToast.LENGTH_LONG, TastyToast.WARNING);
             }
         });
