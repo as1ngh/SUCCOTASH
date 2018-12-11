f087ef8b59e04235c6bd9bb2f9cc1e3d54925803
anupam1262000 <anupam.1262000@gmail.com>
2018-12-11 05:59:29 +0530

    wierd design

--- app/src/main/java/com/mapuna/com/succotash/mycustomadapter.java
+++ app/src/main/java/com/mapuna/com/succotash/mycustomadapter.java
@@ -1,6 +1,7 @@
 package com.mapuna.com.succotash;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -34,6 +35,7 @@ public class mycustomadapter extends ArrayAdapter<File> {
         View view =inflater.inflate(R.layout.customlayout,null);
 
         TextView songname=view.findViewById(R.id.songn);
+
 
         File song=songl.get(position);
         songname.setText(song.getName());
